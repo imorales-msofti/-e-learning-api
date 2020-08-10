2a98d74a3cc26cdef37bf6663574d6cddc570d0f
Ivan Morales <imorales@msofti.com>
2020-08-10 10:31:35 -0500

    CRUD Courses

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import CourseSerializer
+from .models import Course
+from rest_framework import viewsets
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = []
--- e_learning_api/settings.py
+++ e_learning_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'apps.courses',
     'apps.lessons',
     'apps.questions',
