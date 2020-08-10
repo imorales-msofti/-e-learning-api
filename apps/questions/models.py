8c741f31d135ca3c6dac5f2ecdcba0d7f0881cef
Ivan Morales <imorales@msofti.com>
2020-08-10 04:35:02 -0500

    Agrega Modelo Questions

--- apps/questions/models.py
+++ apps/questions/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+from apps.lessons.models import Lesson
+
 # Create your models here.
+class Question(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    question_type = models.CharField(max_length=50, choices=[
+        ('boolean', 'Boolean'),
+        ('only_one', 'Only one answer is correct'),
+        ('more_than_one', 'More than one answer is correct'),
+        ('more_than_one_all_must_correctly', 'More than one answer is correct and all of them must be answered correctly'),
+    ])
+    choises = models.CharField(max_length=50, choices=[
+        ('boolean', 'Boolean'),
+        ('only_one', 'Only one answer is correct'),
+        ('more_than_one', 'More than one answer is correct'),
+        ('more_than_one_all_must_correctly', 'More than one answer is correct and all of them must be answered correctly'),
+    ])
+    correct_answer = models.CharField(max_length=200)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+
+class Answer(models.Model):
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    answer = models.CharField(max_length=200)
+    correct = models.BooleanField()
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f'{question.name}: {self.answer}'
+
+
+
--- e_learning_api/settings.py
+++ e_learning_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.courses',
     'apps.lessons',
+    'apps.questions',
 ]
 
 MIDDLEWARE = [
