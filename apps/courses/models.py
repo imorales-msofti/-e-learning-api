0fc9882b8c7e2c264a4d29c00bf656dda322a8d5
Ivan Morales <imorales@msofti.com>
2020-08-10 03:34:55 -0500

    Agrega modelo Courses

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    approved = models.BooleanField(default=False)
+    previous_course = models.ForeignKey('Course', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{name}'
